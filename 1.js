06eede3b6996aaf89af7268b1cde264060575710
Madhan-509 <150217933+Madhan-509@users.noreply.github.com>
2023-11-08 11:01:45 +0530

    Add files via upload
    This is my first repositary

---
+++ 1.js
@@ -0,0 +1,15 @@
+function man(){
+    document.getElementById("demo").innerHTML=105-6;
+}
+alert("Danager");
+let x,y,z;
+x=10;
+y=20;
+z=x+y;
+console.timeLog(5-66);
+function mat(){
+let X,Y,Z;
+ X=20;
+ Y=30;
+document.getElementById("1,2,3").innerHTML=X+Y; 
+}
